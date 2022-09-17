148ac98809b7f0769232ac5f6c335a2d66f88644
OlachiEfika <38718817+OlachiEfika@users.noreply.github.com>
2022-09-17 14:07:39 -0400

    Update and rename Odinlearning.py to Odinlearning.sh

---
+++ Odinlearning.sh
@@ -0,0 +1,13 @@
+# Documenting my learning on Git/Github 
+echo 'Today, I learnt how to create a new repository using the Github UI' 
+echo 'I also learnt how to connect the repository I created on Github to my local machine using the git clone command'
+echo 'I checked that I had successfully cloned the new repositiory to my local machine by running the cd command into the git_test folder that was downloaded and entered git remote -v on the command line')
+echo 'This displayed the URL of the repository I created on GitHub, which is the remote for my local copy'
+echo 'I created a new file using the touch command'
+echo 'I added the new .txt file to my staging area using the git add command'
+echo 'I checked the status of my add using the git status command, this showed that my file was in the staging area'
+echo 'I configured my email and username details using the git config --global command'
+echo 'I committed the changes to the file using the git commit -m "....." command with an appropriate commit message'
+echo 'I used the git log command to comfirm the entry I committed. This also showed details of the author of the commit, the date and time the commit was made'
+echo 'I pushed the changes made on my local machine to my Github repository using the git push origin main command'
+echo 'I confirmed that the file I updated using the command line had been succesfully pushed to my Github repository'
